0bfec80d3c7894d14df634051766b7e158853353
Will <Will@willspics.co.uk>
2012-06-06 15:10:21 +0100

    Got it creating basic rewrite rules.  Just need to test it with nginx now

---
+++ README.markdown
@@ -0,0 +1,15 @@
+#wp2oct-links#
+
+A small python script that to inspect an exported wordpress XML file and generate nginx rewrite rules for old urls to the default octopress style.  It creates the rewrites as permanent redirects.
+
+It outputs the rules to stdout so you can copy/paste or whatever into the relevant nginx conf.
+
+##Usage##
+    python wp2oct-link.py /path/to/workpress-export.xml
+
+##Installation##
+This app requires lxml and slugify.  You can install them manually or do:
+    pip install -r requirements.txt
+
+##Feedback##
+Please refer to http://github.com/dotnetwill/wp2oct-links
--- wp2oct-link.py
+++ wp2oct-link.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as et
 import sys
+import slugify
 from datetime import datetime
 
 xml_wp_ns = '{http://wordpress.org/export/1.1/}' 
@@ -9,24 +10,26 @@ def generate_from_file(file_path):
         xml_str = f.read()
     root = et.fromstring(xml_str )
     channel = root.find('channel')
+    site_url = channel.find('link').text
     for item in channel.iterfind('item'):
-        print create_url(item)
+        if item.find(xml_wp_ns + 'status').text.lower() == 'publish':
+            create_url(item, site_url)
 
-def create_url(xml_item):
+def create_url(xml_item, site_url):
     title = xml_item.find('title').text
+    title = unicode(title)
     old_link = xml_item.find('link').text
+    old_link = old_link.replace(site_url, '')
     post_date_str = xml_item.find(xml_wp_ns + 'post_date').text
     post_date = datetime.strptime(post_date_str, '%Y-%m-%d %H:%M:%S')
 
-    new_link = "/%s/%s/%s/%s" % (post_date.year, post_date.month, post_date.day, 
-            get_ocotoed_title(title))
+    wp_id = xml_item.find(xml_wp_ns + 'post_id').text
 
-    return '%s -> %s ' % (old_link, new_link)
+    new_link = "/blog/%s/%s/%s/%s/" % (post_date.year, post_date.month, post_date.day, 
+            slugify.slugify(title))
 
-def get_ocotoed_title(title):
-    return title.replace(' ', '-').replace('"', '').replace('#', '') \
-                .replace(':', '').replace('.','').replace('\'', '') \
-                .replace(',', '').replace('"', '')
+    print('rewrite ^{}$ {} permanent;'.format('/?p=' + wp_id, new_link))
+    print('rewrite ^{}$ {} permanent;'.format(old_link, new_link))
 
 if __name__ == '__main__':
     if not sys.argv > 1:
